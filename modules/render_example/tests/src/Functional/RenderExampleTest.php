530b3eecf1f0a02408d444bc3086ac1551a821a0
Qiangjun Ran <33542-jungle@users.noreply.drupalcode.org>
2023-02-18 14:54:15 +0800

    Issue #3342858 by jungle: Fix broken test ::testRenderExample() in render_example

--- modules/render_example/tests/src/Functional/RenderExampleTest.php
+++ modules/render_example/tests/src/Functional/RenderExampleTest.php
@@ -31,7 +31,7 @@ class RenderExampleTest extends BrowserTestBase {
    * Most of our render array altering behaviors are dependent on the standard
    * installation profile, so we'll use that here.
    */
-  public $profile = 'standard';
+  protected $profile = 'standard';
 
   /**
    * Functional tests of the pages we made.
@@ -57,32 +57,34 @@ class RenderExampleTest extends BrowserTestBase {
     // just installed render_example and the config defaults to FALSE for all
     // the alter options, we shouldn't have to manage state before making
     // assertions.
-    $breadcrumb_xpath = "//main[@id='content']//div[contains(@class, 'block-system-breadcrumb-block')]";
+    $breadcrumb_xpath = "//main//div[@id='block-stark-breadcrumbs']";
     $this->assertEmpty($this->xpath($breadcrumb_xpath));
     // Move the breadcrumbs to content region.
     $this->drupalGet($altering_url);
     $this->submitForm(['render_example_move_breadcrumbs' => TRUE, 'render_example_reverse_sidebar' => FALSE, 'render_example_wrap_blocks' => FALSE], 'Save configuration');
+    $session->pageTextContains('The configuration options have been saved.');
     $this->assertNotEmpty($this->xpath($breadcrumb_xpath));
 
-    // Test reversing order of items in region sidebar-first. Get the node
-    // elements under the sidebar region div.
-    $breadcrumb_xpath = "//div[contains(@class,'region-sidebar-first')]/*";
+    // Test reversing order of items in .layout-sidebar-first. Get the node
+    // elements under .layout-sidebar-first.
+    $breadcrumb_xpath = "//aside[contains(@class,'layout-sidebar-first')]/div/*";
     $elements = $this->xpath($breadcrumb_xpath);
-    // There should be two elements, a div and then a nav.
-    $this->assertEquals('div', $elements[0]->getTagName());
-    $this->assertTrue($elements[0]->hasClass('block-search'));
-    $this->assertEquals('nav', $elements[1]->getTagName());
+    // There should be 3 elements.
+    $this->assertEquals('block-stark-page-title', $elements[0]->getAttribute('id'));
+    $this->assertEquals('block-stark-powered', $elements[1]->getAttribute('id'));
+    $this->assertEquals('block-stark-syndicate', $elements[2]->getAttribute('id'));
+
     $this->drupalGet($altering_url);
     $this->submitForm(['render_example_move_breadcrumbs' => FALSE, 'render_example_reverse_sidebar' => TRUE, 'render_example_wrap_blocks' => FALSE], 'Save configuration');
     // Get the elements again.
     $elements = $this->xpath($breadcrumb_xpath);
-    // There should be two elements, a nav and then a div.
-    $this->assertEquals('nav', $elements[0]->getTagName());
-    $this->assertEquals('div', $elements[1]->getTagName());
-    $this->assertTrue($elements[1]->hasClass('block-search'));
+    // There should be 3 reversed elements.
+    $this->assertEquals('block-stark-page-title', $elements[2]->getAttribute('id'));
+    $this->assertEquals('block-stark-powered', $elements[1]->getAttribute('id'));
+    $this->assertEquals('block-stark-syndicate', $elements[0]->getAttribute('id'));
 
     // Test wrapping blocks in divs.
-    $xpath = "//div[contains(@class,'block')]//div[@class='content']/div[@class='block-prefix']";
+    $xpath = "//div[@class='block-prefix']";
     $this->assertEmpty($this->xpath($xpath));
     $this->drupalGet($altering_url);
     $this->submitForm(['render_example_move_breadcrumbs' => FALSE, 'render_example_reverse_sidebar' => FALSE, 'render_example_wrap_blocks' => TRUE], 'Save configuration');
